06feaca621f808353e86718119e3d390c8d54388
arjun568 <arjuntiwari568@gmail.com>
2025-05-26 23:53:43 +0530

    Radio and Dropdown commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,39 @@ function App() {
   <Home/>
   <Counter/>
   <Toggle/>
+  <PropsTaskClock/>
+  <Skills/>
   <PropsTaskClock/>*/
   <>
-  <Skills/>
-  <PropsTaskClock/>
+  <radioDropdown/>
   </>
+  )
+}
+
+function radioDropdown() {
+
+  const [gender,setGender]=useState('female');
+  const [city,setCity]=useState('delhi');
+  return (
+    <div>
+      <h1>Handle Radio and Dropdown</h1>
+      <h4>Select Gender</h4>
+      <input type="radio" onChange={(event)=>setGender(event.target.value)} name="gender" 
+       value={"male"} checked={gender == 'male'} id="male" />
+      <label htmlFor="male">Male</label>
+      <input type="radio" onChnage={(event)=>setGender(event.target.value)} name="gender" 
+       value={"female"} checked={gender == 'female'} id="female" />
+      <label htmlFor="female">Female</label>
+      <h2>Selected Gender :{gender}</h2>
+      <br /><br /><br />
+      <h4>Select City</h4>
+      <select onChange={(event)=>setCity(event.target.value)} defaultValue={"delhi"}>
+        <option value="noida">Noida</option>
+        <option value="gurgaon">Gurgaon</option>
+        <option value="delhi">Delhi</option>
+      </select>
+      <h2>Selected City : {city}</h2>
+    </div>
   )
 }
 
